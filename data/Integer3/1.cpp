28175399cb48339222953751876a8755fcac36fe
DarkSharpness_XzY <2040703891@qq.com>
2023-10-20 22:07:15 +0800

    Upd: 加入了一些非常毒瘤的 corner case 测试点。

---
+++ data/Integer3/1.cpp
@@ -0,0 +1,42 @@
+/*
+Time: 2023-10-20
+Test: Custom constructor. Div and mod.
+std Time: 0.30s
+Time Limit: 1.00s
+Notes: Sanity check.
+*/
+
+#include "int2048.h"
+
+void sanity_check(sjtu::int2048 &x,sjtu::int2048 &y) {
+    // Sanity check of division
+    std::cout
+        << x / y    << ' '
+        << -x / y   << ' '
+        << x / -y   << ' '
+        << -x / -y  << '\n';
+
+    // Sanity check of mod
+    std::cout
+        << x % y    << ' '
+        << -x % y   << ' '
+        << x % -y   << ' '
+        << -x % -y  << '\n';
+}
+
+signed main() {
+    // sanity check
+    sjtu::int2048 x {"0000000000001145141919810"};
+    // normal test
+    sjtu::int2048 y {"-1145141919810"};
+
+    y += 1;
+    sanity_check(x, y);
+
+    y += -2;
+    y -= -1;
+
+    sanity_check(x, y);
+
+    return 0;
+}
---
+++ data/Integer3/3.cpp
@@ -0,0 +1,26 @@
+/*
+Time: 2023-10-20
+Test: Copy constructor and assignment operator.
+std Time: 0.30s
+Time Limit: 1.00s
+Notes: Sanity check.
+*/
+
+#include "int2048.h"
+
+void sanity_check(sjtu::int2048 &x) {
+    x = x = x = x = x = x = x = x = x;
+    sjtu::int2048 y = (((((((((((((x)))))))))))));
+    sjtu::int2048 z = (((((((-(((((x))))))))))));
+    x = ( x = (x = (x = y + z + x)));
+}
+
+signed main() {
+    sjtu::int2048 x {"111111111111111111111111111111111111111111111111111111111111111111111111112"};
+    sanity_check(x);
+    std::cout << x << '\n';
+    std::cout << -sjtu::int2048{0} << '\n';
+    x.read("-0");
+    std::cout << x << '\n';
+    return 0;
+}
